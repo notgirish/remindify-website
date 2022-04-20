014cbfe40b43e9326e96e5c8b9c218f98f05bddd
notgirish <itsnotgirish@gmail.com>
2022-04-20 19:22:13 +0530

    hero vid fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import video from '../assets/herovid.mp4'
 
 export default function Hero() {
     return (
         <section className='container flex space-x-14 items-center'>
             <video width="540" height="540" muted="" playsinline="" preload="metadata" poster="" autoPlay="true" className='rounded-2xl'>
-                <source src={video} type="video/mp4" />
+                <source src={require('../assets/herovid.mp4')} type="video/mp4" />
             </video>
             <div>
                 <h1 className='text-7xl text-secondary font-[ttinterfaces] font-bold'>Remindify. <br/><span className='text-customgray'>Notify tasks to yourself and stay organized.</span></h1>
