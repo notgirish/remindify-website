c42337e93306809c1db503a107e4df4aa40bf5c3
notgirish <itsnotgirish@gmail.com>
2022-04-20 19:01:34 +0530

    know more page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import ArrowRightSVG from '../assets/ArrowRightSVG'
 export default function HeaderNav() {
     return (
         <nav>
-            <div className='container flex justify-between items-center'>
+            <div className='container flex justify-between items-center px-4 py-4'>
                 <img src={require('../assets/logo.png')} alt='remindify logo' className='w-16'/>
-                <div className='bg-secondary text-primary flex items-center px-4 rounded-2xl'>
-                    <a href='/' className='flex h-12 w-max items-center font-[ttinterfaces] font-bold text-lg'>Get in now &nbsp;</a>
+                <div className='bg-secondary text-primary flex items-center px-4 rounded-2xl mr-3'>
+                    <a href='/' className='flex h-12 w-max items-center font-[ttinterfaces] font-bold text-lg'>Get it now &nbsp;</a>
                     <ArrowRightSVG width={22} height={22} />
                 </div>
             </div>
--- src/pages/Knowmore.js
+++ src/pages/Knowmore.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Knowmore() {
   return (
-    <div>Knowmore</div>
+    <main className='container px-7 py-4'>
+      <p className='text-5xl text-secondary font-[ttinterfaces] font-bold pb-7'>Thank you for downloading this app. <span className='text-customgray'>Hopefully it has helped you in increasing your productivity.</span></p>
+
+      <svg width="41" height="41" viewBox="0 0 82 82" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M38.284 8.85583C39.3643 6.55645 42.6351 6.55691 43.7148 8.85658L52.0413 26.5913C52.4657 27.4952 53.3122 28.1288 54.2991 28.2811L73.1635 31.1936C75.5752 31.566 76.557 34.5085 74.8522 36.2544L61.022 50.4182C60.3559 51.1004 60.0538 52.0585 60.208 52.9994L63.4576 72.8206C63.8629 75.2932 61.238 77.1438 59.0453 75.9313L42.4732 66.7667C41.5705 66.2676 40.4746 66.2671 39.5716 66.7656L22.97 75.9295C20.7774 77.1398 18.1546 75.2892 18.5598 72.8177L21.8103 52.9909C21.9646 52.0498 21.6622 51.0913 20.9957 50.4091L7.15039 36.2367C5.44494 34.4909 6.42663 31.5478 8.83858 31.1754L27.7018 28.2631C28.6885 28.1107 29.5347 27.4775 29.9593 26.5739L38.284 8.85583Z" fill="#F5D370" />
+      </svg>
+      <p className='text-2xl text-secondary font-[ttinterfaces] font-bold'> If you've liked the app, it'd be great if you could rate it 5 stars on the Google Playstore.</p>
+    </main>
   )
 }
