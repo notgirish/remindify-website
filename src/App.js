307c30277a1b75364b706e74d79753c4a75f043e
notgirish <itsnotgirish@gmail.com>
2022-04-20 20:02:27 +0530

    know more page fix 4

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Homepage from './pages/Homepage';
 import Knowmore from './pages/Knowmore';
 
 export default function App() {
   return (
-    <BrowserRouter>
-
+    <>
       <Header />
-
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/more" element={<Knowmore />} />
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/more" element={<Knowmore />} />
       </Routes>
-
       <Footer />
-
-    </BrowserRouter>
+    </>
   )
 }
