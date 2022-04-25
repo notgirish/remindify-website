b9c366f5b64a8a80573bf878faccaa2d4bcc5acf
notgirish <itsnotgirish@gmail.com>
2022-04-25 15:08:42 +0530

    playstore badge fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
             <section className='container md:flex md:items-center'>
                 <div>
                     <h1 className='text-5xl text-secondary font-bold mb-1 xl:text-7xl mt-8'>Remify. <br /><span className='features-explanation'>Notify tasks to yourself and stay organized.</span></h1>
-                    <a href="https://play.google.com/store/apps/details?id=com.puzolve.remify" target="_blank" rel="noreferrer"><img src={require('../assets/gplay-badge.png')} alt='download app on google playstore' className='w-44 mt-7 h:w-64 xl:mt-12'/></a>
+                    <a href="https://play.google.com/store/apps/details?id=com.puzolve.remify" target="_blank" rel="noreferrer"><img src={require('../assets/gplay-badge.png')} alt='download app on google playstore' className='w-44 mt-7 xl:w-64 xl:mt-12'/></a>
                 </div>
                 <video playsInline="" preload="metadata" poster="" autoPlay={true} muted className='rounded-[2rem] mt-12 md:order-first w-[300px] md:min-w-[340px] xl:min-w-[500px] max-w-[540px] md:mr-7 xl:mr-14'>
                     <source src={heroVid} type="video/mp4" />
