c34b4d25978b7fad8ba8e80749afba5811693b40
notgirish <itsnotgirish@gmail.com>
2022-04-25 15:18:08 +0530

    mobile 404 page fix

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function PageNotFound() {
     return (
-        <main className='px-6 flex flex-col justify-center xl:snap-start xl:h-screen'>
+        <main className='px-6 flex flex-col justify-center xl:snap-start h-screen'>
             <section className='container'>
                 <div>
-                    <h1 className='text-5xl text-secondary font-bold mb-1 xl:text-7xl mt-8'>404 error. <br /><span className='features-explanation'>(╥︣﹏᷅╥) umm... this page might not exist.</span></h1>
+                    <h1 className='text-5xl text-secondary font-bold mb-1 xl:text-7xl mt-8'>404 error. <br /><span className='features-explanation'>(⌐■_■) umm... this page might not exist.</span></h1>
                 </div>
             </section>
         </main>
