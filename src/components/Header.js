3d901a4a2f948279f59a23289a23476d35db2140
notgirish <itsnotgirish@gmail.com>
2022-04-26 23:14:13 +0530

    updated the logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ArrowRightSVG from '../assets/ArrowRightSVG'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo-flat.svg'
 
 export default function HeaderNav() {
     return (
         <nav className='fixed z-10 h-24 w-full bg-white'>
             <div className='container flex justify-between items-center px-4 py-4'>
-                <Link to="/"><img src={require('../assets/logo.png')} alt='remify logo' className='w-16'/></Link>
+                <Link to="/"><img src={logo} alt='remify logo' className='w-12'/></Link>
                 <div className='bg-secondary text-primary flex items-center px-4 rounded-2xl mr-3'>
                     <a href="https://play.google.com/store/apps/details?id=com.puzolve.remify" target="_blank" rel="noreferrer" className='flex h-12 w-max items-center font-bold text-lg'>Get it now &nbsp;</a>
                     <ArrowRightSVG width={22} height={22} />
